827d4dfb061e5d36394f3702be1ae1521afef752
Kanrawee Karaboot <tulakan17@gmail.com>
2017-06-13 22:02:58 +0700

    MQTT AND BLE

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var noble = require('noble');
-noble.on('stateChange', function (state) {
-    if (state === 'poweredOn') {
-        noble.startScanning();
-    } else {
-        noble.stopScanning();
-    }
-});
-
-
+var mqtt = require('mqtt')
+var options = {
+    port: 16317,
+    host: 'mqtt://m12.cloudmqtt.com',
+    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    username: 'hduypxmn',
+    password: '0lJtYsmmA8Pm',
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+};
+var client = mqtt.connect('mqtt://m12.cloudmqtt.com', options)
 var noble = require('noble');
 noble.on('stateChange', function (state) {
     console.log('state : ' + state);
@@ -18,54 +24,46 @@ noble.on('stateChange', function (state) {
     }
 });
 
+client.on('connect', function () {
+    client.subscribe('presence')
+    console.log("subscribed")
+    //   client.publish('presence', 'Hello mqtt')
+})
 
-noble.on('discover', function (peripheral) {
-    //   console.log('peripheral discovered (' + peripheral.id +
-    //               ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
-    //               ' connectable ' + peripheral.connectable + ',' +
-    //               ' RSSI ' + peripheral.rssi + ':');
-    //   console.log('\thello my local name is:');
-    //   console.log('\t\t' + peripheral.advertisement.localName);
-    //   console.log('\tcan I interest you in any of the following advertised services:');
-    //   console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log("message :", message.toString())
+    if (message.toString() == 'scan') {
+        console.log("Scaning for BLE Device")
+        noble.on('discover', function (peripheral) {
+            console.log("on discover");
+            if (peripheral.address == 'e9:85:a4:34:d0:2a') {
+                console.log("correct");
+                console.log("TIME: " + new Date().getTime());
+                console.log('on -> Name: ' + peripheral.advertisement.localName);
+                console.log('on -> Address: ' + peripheral.address);
 
-    //   var serviceData = peripheral.advertisement.serviceData;
-    //   if (serviceData && serviceData.length) {
-    //     console.log('\there is my service data:');
-    //     for (var i in serviceData) {
-    //       console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': ' + JSON.stringify(serviceData[i].data.toString('hex')));
-    //     }
-    //   }
-    //   if (peripheral.advertisement.manufacturerData) {
-    //     console.log('\there is my manufacturer data:');
-    //     console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
-    //   }
-    //   if (peripheral.advertisement.txPowerLevel !== undefined) {
-    //     console.log('\tmy TX power level is:');
-    //     console.log('\t\t' + peripheral.advertisement.txPowerLevel);
-    //   }
+                var UUID = peripheral.advertisement.manufacturerData.slice(4, 20);
+                console.log('on -> UUID: ' + toHexString(UUID));
 
-    //   console.log();
-    // console.log("on discover");
-    if (peripheral.address == 'e9:85:a4:34:d0:2a') {
-        console.log("TIME: " + new Date().getTime());
-        console.log('on -> Name: ' + peripheral.advertisement.localName);
-        console.log('on -> Address: ' + peripheral.address);
+                var Mojor = peripheral.advertisement.manufacturerData.slice(20, 22);
+                console.log('on -> Major: ' + toHexString(Mojor));
 
-        var UUID = peripheral.advertisement.manufacturerData.slice(4, 20);
-        console.log('on -> UUID: ' + toHexString(UUID));
-
-        var Mojor = peripheral.advertisement.manufacturerData.slice(20, 22);
-        console.log('on -> Major: ' + toHexString(Mojor));
-
-        Minor = peripheral.advertisement.manufacturerData.slice(22, 24);
-        console.log('on -> Minor: ' + toHexString(Minor));
-        console.log('=======================================');
-
+                Minor = peripheral.advertisement.manufacturerData.slice(22, 24);
+                console.log('on -> Minor: ' + toHexString(Minor));
+                console.log('=======================================');
+            } else {
+                console.log("incorrect !");
+            }
+        });
     }
+    //   client.end()
+})
 
 
-});
+
+
+
 
 // Functions Convert Byte to HexString
 function toHexString(arr) {
---
+++ pub.js
@@ -0,0 +1,28 @@
+var mqtt = require('mqtt')
+var options = {
+    port: 16317,
+    host: 'mqtt://m12.cloudmqtt.com',
+    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    username: 'hduypxmn',
+    password: '0lJtYsmmA8Pm',
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+};
+var client = mqtt.connect('mqtt://m12.cloudmqtt.com', options)
+
+client.on('connect', function () {
+    // client.subscribe('presence')
+    // console.log("subscribed")
+    client.publish('presence', 'scan')
+    client.end()
+})
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log("message :", message.toString())
+    
+})
