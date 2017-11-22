550f7a9405046023d9d18b355458cd632df898ce
Kanrawee Karaboot <tulakan17@gmail.com>
2017-11-22 10:27:23 +0700

    add mqtt

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var noble = require('noble');
 var mqtt = require('mqtt')
+
+
+//CLI argument
+var classIdx = process.argv[2]
+var roomIdx = process.argv[3]
+console.log('classroom --> ', classIdx, 'roomIdx --> ', roomIdx);
+console.log('new upload')
+
+//mqtt options 
 var options = {
     port: 16317,
     host: 'mqtt://m12.cloudmqtt.com',
@@ -14,49 +23,139 @@ var options = {
     encoding: 'utf8'
 };
 var client = mqtt.connect('mqtt://m12.cloudmqtt.com', options)
+
 var noble = require('noble');
+
 noble.on('stateChange', function (state) {
     console.log('state : ' + state);
     if (state === 'poweredOn') {
-        noble.startScanning([], true);
-        console.log("scanning");
+        // noble.startScanning([], true);
+        // console.log("scanning");
     } else {
-        noble.stopScanning();
-        console.log("stop scan");
+        // noble.stopScanning();
+        // console.log("stop scan");
     }
 });
 
 client.on('connect', function () {
-    client.subscribe('gateway-ble')
-    console.log("subscribed")
+    client.subscribe('gateway-ble/' + classIdx + '/' + roomIdx)
+    console.log('subscribed in topic : gateway-ble/' + classIdx + '/' + roomIdx)
 })
 
-var scanState = false;
-var UUIDList = [];
+noble.on('discover', function (peripheral) {
+    console.log('on discover');
+    var address = peripheral.address;
+    var Mojor = peripheral.advertisement.manufacturerData.slice(20, 21);
+    // var Mojor = MojorBuffer[MojorBuffer.le-1]
 
-client.on('message', function (topic, message) {
-    // message is Buffer
-    var scandata = JSON.parse(message);
-    // console.log("message :", message.toString());
-    // var massageObject = message;
-    scanState = scandata.scanState;
-    console.log('UUIDList.length --> ', UUIDList.length);
-    if (scanState) {
-        UUIDList = scandata.deviceUUID;  
-
-        //todo test scan device in uuidList
-        noble.on('discover', function (peripheral) {
-            UUIDList.forEach((deviceUUID) => {
-                if (peripheral.address == deviceUUID) {
-                    var Mojor = peripheral.advertisement.manufacturerData.slice(20, 22);
-                    console.log('deviceUUID : ', deviceUUID, ' Answer is : ' + toHexString(Mojor));
-                }
-            })
-        });
-        //todo test scan device in uuidList
+    if (peripheral.address == 'e9:85:a4:34:d0:2a') {
+        console.log('----------------------------------------------------------------------------------');
+        console.log('address : ', address, ' Answer is : ' + toHexString(Mojor));
+        console.log('Mojor -- >', peripheral.major);
+        console.log('----------------------------------------------------------------------------------');
     } else {
-        console.log('not scan');
+        console.log('address : ', address, ' Answer is : ' + toHexString(Mojor));
     }
+});
+
+var scanState = false;
+var UUIDList;
+
+client.on('message', function (topic, data) {
+
+    console.log('typeof data ----> ', typeof data);
+    if (typeof data === 'string') {
+        var parseedData = data
+    } else {
+        var parseedData = JSON.parse(data);
+    }
+    // console.log('on message arrive : parseedData -> ', parseedData);
+
+
+    //if check state
+    if (parseedData == 'checkBLEScanState') {
+        console.log('on GW check state');
+        var sendStateTopic = 'gateway-ble/' + classIdx + '/' + roomIdx + '/checkBLEScanState'
+        console.log('sendStateTopic --> ', sendStateTopic);
+        var bleState = noble.state;
+        console.log('bleState --> ', bleState);
+        if (bleState == 'poweredOn') {
+            client.publish(sendStateTopic, 'ready');
+        } else {
+            client.publish(sendStateTopic, 'not ready');
+        }
+    }
+
+    if (parseedData.message != undefined && parseedData.message == 'sendingMacAdressDATA') {
+        console.log('on sendingMacAdressDATA');
+        UUIDList = parseedData.macAddressList
+        console.log('UUIDList --> ', UUIDList);
+    }
+
+
+    if (parseedData.message != undefined && parseedData.message == 'startScan') {
+        console.log('on startScan');
+        var bleState = noble.state;
+        if (bleState == 'poweredOn') {
+            noble.startScanning([], true);
+        } else {
+            noble.stopScanning();
+        }
+    }
+
+    if (parseedData.message != undefined && parseedData.message == 'stopScan') {
+        console.log('on stopScan');
+        var bleState = noble.state;
+        if (bleState == 'poweredOn') {
+            noble.stopScanning();
+        } else {
+            noble.stopScanning();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // message is Buffer
+    // var scandata = JSON.parse(message);
+    // // console.log("message :", message.toString());
+    // // var massageObject = message;
+    // scanState = scandata.scanState;
+    // console.log('UUIDList.length --> ', UUIDList.length);
+    // console.log('UUIDList --> ', UUIDList);
+    // if (scanState) {
+    //     UUIDList = scandata.deviceUUID;
+
+    //     //todo test scan device in uuidList
+    //     noble.on('discover', function (peripheral) {
+    //         UUIDList.forEach((deviceUUID) => {
+    //             if (peripheral.address == deviceUUID) {
+    //                 var Mojor = peripheral.advertisement.manufacturerData.slice(20, 22);
+    //                 console.log('deviceUUID : ', deviceUUID, ' Answer is : ' + toHexString(Mojor));
+    //             }
+    //         })
+    //     });
+    //     //todo test scan device in uuidList
+    // } else {
+    //     console.log('not scan');
+    // }
 })
 
 
---
+++ testScan.js
@@ -0,0 +1,63 @@
+var noble = require('noble');
+var Minor = "";
+var a = "";
+console.log('Program Start....');
+
+
+// Functions Convert Byte to HexString
+function toHexString(arr) {
+    var str = '';
+    for (var i = 0; i < arr.length; i++) {
+        str += ((arr[i] < 16) ? "0" : "") + arr[i].toString(16);
+        str += ":";
+    }
+    return str;
+}
+
+
+noble.on('stateChange', function (state) {   // Event การเปลี่ยนแปลงสถานะของ Bluetooth เมื่อโปรแกรมสั่งเปิด
+    console.log('on -> stateChange: ' + state);
+    console.log('Bluetooth ON...');
+    if (state === 'poweredOn') {
+        noble.startScanning([], true);   // เมื่อ Bluetooth On แล้วให้ทำการสั่ง Scan ||  [] any service UUID, allow duplicates, true ค้นหาแบบไม่กำหนดเวลา
+        console.log('Bluetooth Scanner...');
+    } else {
+        noble.stopScanning();
+        console.log('Not working...');
+    }
+});
+
+noble.on('scanStart', function () {
+    console.log('on -> scanStart');
+});
+
+noble.on('scanStop', function () {
+    console.log('on -> scanStop');
+});
+
+
+
+noble.on('discover', function (peripheral) {  // return BLE Device ที่อยู่รอบๆ
+
+    if (peripheral.address == 'e9:85:a4:34:d0:2a') {
+        console.log("TIME: " + new Date().getTime());
+        console.log('on -> Name: ' + peripheral.advertisement.localName);
+        console.log('on -> Address: ' + peripheral.address);
+
+        // var UUID = peripheral.advertisement.manufacturerData.slice(4, 20);
+        // console.log('on -> UUID: ' + toHexString(UUID));
+
+        var Mojor = peripheral.advertisement.manufacturerData.slice(20, 22);
+        console.log('on -> Major: ' + toHexString(Mojor));
+
+        // Minor = peripheral.advertisement.manufacturerData.slice(22, 24);
+        // console.log('on -> Minor: ' + toHexString(Minor));
+        // console.log('=======================================');
+        // if ((toHexString(Minor).localeCompare('00:14:')) == 0) {
+        //     message = "Standby..";
+        // } else if ((toHexString(Minor).localeCompare('00:15:')) == 0) {
+        //     message = "Run!!";
+        // }
+        // console.log('Message --> ' + message);
+    }
+});
